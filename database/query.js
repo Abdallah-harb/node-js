f5f7c371b337bd7cc80490eaba743824c8c97b03
Abdallah-harb <abdallahabdelrahman186@gmail.com>
2025-01-29 13:10:34 +0200

    configuration postgress db

---
+++ database/query.js
@@ -0,0 +1,12 @@
+const pool = require('./pool');
+exports.query =(queryText,queryParams)=>{
+        return new Promise((resolve ,reject)=>{
+            pool.query(queryText,queryParams)
+                .then(res =>{
+                    resolve(res);
+                })
+                .catch(error =>{
+                    reject(error);
+                });
+        });
+}
